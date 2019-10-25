bd7dbfd2a7d90583e7af26c25697385fc5f71233
LAPTOP-QQULPCJ7\edyta <edyta.sleziak@gmail.com>
2019-10-25 21:24:28 +0100

    Adding image

--- app/src/main/java/org/wit/hillforts/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillforts/activities/HillfortActivity.kt
@@ -1,5 +1,6 @@
 package org.wit.hillforts.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -11,29 +12,41 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.hillforts.R
+import org.wit.hillforts.helpers.readImage
+import org.wit.hillforts.helpers.readImageFromPath
+import org.wit.hillforts.helpers.showImagePicker
 import org.wit.hillforts.main.MainApp
 import org.wit.hillforts.models.HillfortModel
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
   var hillfort = HillfortModel()
+  val IMAGE_REQUEST = 1
   lateinit var app: MainApp
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_hillfort)
+    toolbarHillfort.title = title
+    setSupportActionBar(toolbarHillfort)
+    chooseImage.setOnClickListener {
+      info ("Select image")
+    }
+
     app = application as MainApp
 
     var edit = false
 
-    toolbarHillfort.title = title
-    setSupportActionBar(toolbarHillfort)
+    chooseImage.setOnClickListener {
+      showImagePicker(this, IMAGE_REQUEST)
+    }
 
     if (intent.hasExtra("hillfort_edit")) {
       edit = true
       hillfort = intent.extras?.getParcelable<HillfortModel>("hillfort_edit")!!
       HillfortName.setText(hillfort.name)
       HillfortDescription.setText(hillfort.description)
+      HillfortImage.setImageBitmap(readImageFromPath(this, hillfort.image))
       btnAdd.setText(R.string.button_editHillfort)
     }
 
@@ -67,4 +80,18 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {
     }
     return super.onOptionsItemSelected(item)
   }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    when (requestCode) {
+      IMAGE_REQUEST -> {
+        if (data != null) {
+          hillfort.image = data.getData().toString()
+          HillfortImage.setImageBitmap(readImage(this, resultCode, data))
+        }
+      }
+    }
+  }
+
+
 }
