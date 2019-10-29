c7e36e447888f2e91dfd6297211e51ac5bfa86cf
Edyta Sleziak <Edyta.Sleziak@fenergo.com>
2019-10-29 14:58:45 +0000

    Splash screen added

---
+++ app/src/main/java/org/wit/hillforts/activities/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package org.wit.hillforts.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.wit.hillforts.R
+
+class SplashScreenActivity : AppCompatActivity() {
+  override fun onCreate(savedInstaceState: Bundle?) {
+    super.onCreate(savedInstaceState)
+    setContentView(R.layout.activity_splashscreen)
+
+    val background = object : Thread() {
+      override fun run() {
+        try {
+          Thread.sleep(5000)
+          startActivity(Intent(baseContext, HillfortsListActivity::class.java))
+        } catch (e: Exception) {
+          e.printStackTrace()
+        }
+      }
+    }
+    background.start()
+  }
+}
