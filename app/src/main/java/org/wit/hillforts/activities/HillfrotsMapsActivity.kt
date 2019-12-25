1e12466104f94aaa8a0a23e49ab9bdd52b51550b
Edyta Sleziak <Edyta.Sleziak@fenergo.com>
2019-12-25 17:18:57 +0000

    Map added

--- app/src/main/java/org/wit/hillforts/activities/HillfrotsMapsActivity.kt
+++ app/src/main/java/org/wit/hillforts/activities/HillfrotsMapsActivity.kt
@@ -1,23 +1,59 @@
 package org.wit.hillforts.activities
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
 import org.wit.hillforts.R
-
 import kotlinx.android.synthetic.main.activity_hillfrots_maps.*
+import kotlinx.android.synthetic.main.activity_map.*
+import kotlinx.android.synthetic.main.content_hillfrots_maps.*
+import org.wit.hillforts.main.MainApp
+
 
 class HillfrotsMapsActivity : AppCompatActivity() {
+  lateinit var app: MainApp
+  lateinit var map: GoogleMap
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_hillfrots_maps)
+    toolbar.title = title
     setSupportActionBar(toolbar)
-
-    fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        .setAction("Action", null).show()
+    mapView.onCreate(savedInstanceState)
+    app = application as MainApp
+    mapView.getMapAsync {
+      map = it
+      configureMap()
     }
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    mapView.onDestroy()
+  }
+
+  override fun onLowMemory() {
+    super.onLowMemory()
+    mapView.onLowMemory()
+  }
+
+  override fun onPause() {
+    super.onPause()
+    mapView.onPause()
+  }
+
+  override fun onResume() {
+    super.onResume()
+    mapView.onResume()
+  }
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    mapView.onSaveInstanceState(outState)
+  }
+
+  fun configureMap() {
+    map.uiSettings.setZoomControlsEnabled(true)
   }
 
 }
